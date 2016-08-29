aeeb65db70dfeda114d88de3003d37743ef71eef
chris48s <c.mshaw@yahoo.co.uk>
2016-08-29 15:36:02 +0100

    init

---
+++ scraper.py
@@ -0,0 +1,40 @@
+from gml_scraper import scrape
+
+
+stations_url = "http://www2.guildford.gov.uk/ishare5.2.web/getows.ashx?service=WFS&version=1.1.0&request=GetFeature&typeName=polling_places&mapsource=GBC/Inspire"
+stations_fields = {
+    '{http://mapserver.gis.umn.edu/mapserver}ogc_fid': 'ogc_fid',
+    '{http://mapserver.gis.umn.edu/mapserver}pollingdistrict': 'pollingdistrict',
+    '{http://mapserver.gis.umn.edu/mapserver}thoroughfare_name': 'thoroughfare_name',
+    '{http://mapserver.gis.umn.edu/mapserver}northing': 'northing',
+    '{http://mapserver.gis.umn.edu/mapserver}pai_update_status': 'pai_update_status',
+    '{http://mapserver.gis.umn.edu/mapserver}pollingplace': 'pollingplace',
+    '{http://mapserver.gis.umn.edu/mapserver}easting': 'easting',
+    '{http://mapserver.gis.umn.edu/mapserver}register': 'register',
+    '{http://mapserver.gis.umn.edu/mapserver}copyright': 'copyright',
+    '{http://mapserver.gis.umn.edu/mapserver}mi_prinx': 'mi_prinx',
+    '{http://mapserver.gis.umn.edu/mapserver}datatablename': 'datatablename',
+    '{http://mapserver.gis.umn.edu/mapserver}datasource': 'datasource',
+}
+
+districts_url = "http://www2.guildford.gov.uk/ishare5.2.web/getows.ashx?service=WFS&version=1.1.0&request=GetFeature&typeName=polling_districts&mapsource=GBC/Inspire"
+districts_fields = {
+    '{http://mapserver.gis.umn.edu/mapserver}ogc_fid': 'ogc_fid',
+    '{http://mapserver.gis.umn.edu/mapserver}borough_ward': 'borough_ward',
+    '{http://mapserver.gis.umn.edu/mapserver}datatablename': 'datatablename',
+    '{http://mapserver.gis.umn.edu/mapserver}constituency': 'constituency',
+    '{http://mapserver.gis.umn.edu/mapserver}pai_update_status': 'pai_update_status',
+    '{http://mapserver.gis.umn.edu/mapserver}pollingdistrictname': 'pollingdistrictname',
+    '{http://mapserver.gis.umn.edu/mapserver}areahectares': 'areahectares',
+    '{http://mapserver.gis.umn.edu/mapserver}parish': 'parish',
+    '{http://mapserver.gis.umn.edu/mapserver}register': 'register',
+    '{http://mapserver.gis.umn.edu/mapserver}county_division': 'county_division',
+    '{http://mapserver.gis.umn.edu/mapserver}mi_prinx': 'mi_prinx',
+    '{http://mapserver.gis.umn.edu/mapserver}copyright': 'copyright',
+}
+
+council_id = 'E07000209'
+
+
+scrape(stations_url, council_id, 'stations', stations_fields, 'ogc_fid')
+scrape(districts_url, council_id, 'districts', districts_fields, 'ogc_fid')
